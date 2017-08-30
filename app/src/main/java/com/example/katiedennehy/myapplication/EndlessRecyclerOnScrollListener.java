9301453e9f11fdc158ab873d9dd23c844e67004c
Roisin Donovan <roisindonovan@Roisins-MacBook-Pro.local>
2017-08-30 20:53:53 +0100

    Initial commit

---
+++ app/src/main/java/com/example/katiedennehy/myapplication/EndlessRecyclerOnScrollListener.java
@@ -0,0 +1,8 @@
+package com.example.katiedennehy.myapplication;
+
+/**
+ * Created by Katie Dennehy on 08/08/2017.
+ */
+
+public class EndlessRecyclerOnScrollListener {
+}
---
+++ app/src/main/java/com/example/katiedennehy/myapplication/Friends.java
@@ -0,0 +1,17 @@
+package com.example.katiedennehy.myapplication;
+
+/**
+ * Created by Katie Dennehy on 19/07/2017.
+ */
+
+public class Friends {
+
+    public String date;
+    public Friends(){
+
+}
+
+public Friends(String date) {this.date=date;}
+public String getDate() {return date;}
+public void setDate(String date) {this.date = date;}
+}
